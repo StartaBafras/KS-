3d51bbbc63afee7a0fb2f91beb5e70150b23ac9e
StartaBafras <30952126+StartaBafras@users.noreply.github.com>
2020-03-23 00:23:52 +0300

    Add files via upload

--- kvi.py
+++ kvi.py
@@ -5,13 +5,23 @@ kalkış="İzmit"
 varış="Karamürsel"
 geçici= []
 istasyondan_geçen=[]
-dere=sqlite3.connect("rıhtım.db")
+dere=sqlite3.connect("/home/beyhan/Belgeler/Python/KDU/rıhtım.db")
 im=dere.cursor()
 im.execute("create table if not exists hatlar('hat','durak1','durak2','durak3','durak4','durak5')")
 im.execute("select * from hatlar")
 tablo=im.fetchall()
+dere.close()
 for hat in tablo:
     for duraklar in hat:
         if duraklar == kalkış:
-            geçici.append(hat)
-
+            geçici.append(hat)#Veri tabanındaki boş hücreleri silmeye gerek var mı ?
+sayaç=len(geçici)-1
+while sayaç > -1:
+    for i in geçici[sayaç]:
+        if i == varış:
+            print(geçici[sayaç][0],"ile gidilir")
+            del geçici[sayaç]
+    sayaç -= 1
+sayaç=len(geçici)-1
+print(i)
+print(geçici)
