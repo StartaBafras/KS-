07157247b58822490c06905a4881e7f7417424bd
StartaBafras <beyhankaradag00@gmail.com>
2020-03-29 21:49:07 +0300

    Bozuk fonksiyon kaldırıldı çıktı ekranı için kalkışların nereden yapılacağı eklendi

--- kvi2.py
+++ kvi2.py
@@ -1,5 +1,4 @@
 import sqlite3
-import denemeler #Böyle yapınca global değişken nedense çalışmıyor fonksiyonu buraya kopyalamak gerekti
 #kalkış=input("Kalkış İskelesi: ")
 #varış=input("Varış İskelesi: ")
 kalkış="Gölcük"
@@ -9,6 +8,8 @@ gidenler= []
 
 def geçenler(istasyon):# istasyondan geçen hatları verir
     import sqlite3
+    global bulunulan_durak
+    bulunulan_durak = istasyon
     dere=sqlite3.connect("/home/beyhan/Belgeler/Python/KDU/rıhtım.db")
     im=dere.cursor()
     #im.execute("create table if not exists hatlar('hat','durak1','durak2','durak3','durak4','durak5')")
@@ -25,30 +26,6 @@ def geçenler(istasyon):# istasyondan geçen hatları verir
     return istasyondan_geçen
 
 
-
-
-def doğrudan(geçen_trenler,hedef):#Hatların hedefe doğrudan gidip gitmediğini kontrol eder
-    sayaç=len(geçen_trenler)-1
-    while sayaç > -1:
-        for i in geçen_trenler[sayaç]:
-            if i == hedef:
-                print(i)
-                print(geçen_trenler[sayaç][0],"ile doğrudan gidilir")
-                print("Kalkış istasyonu:{} \n Varış istasyonu: {}".format(kalkış,varış))
-                del geçen_trenler[sayaç]
-                #sayaç=len(geçen_trenler)-1
-                print(geçen_trenler)
-            if i == NameError:
-                sayaç -= 1 
-            #else:
-                #print(sayaç)
-                #print(i)
-                #print(geçen_trenler[sayaç][0],"ile doğrudan gidilmez")
-        #sayaç -= 1
-        #print(sayaç)
-    del i
-
-
 ilk_durak = []
 ilk_durak = geçenler(kalkış)
 
@@ -60,15 +37,15 @@ def aktarma(aktarma_yapabilecekler):
             durak_2 = geçenler(duraklar)
             doğrudan(durak_2,varış)
 
-def doğrudan2(geçen_trenler,varış):
+def doğrudan2(bulunulan_durak,geçen_trenler,varış):
     for hat in geçen_trenler:
         for duraklar in hat:
             if duraklar == varış:
-                print(hat[0],"doğrudan gider")
+                print(hat[0],"Bulunulan durak: {} Varılan durak: {}".format(bulunulan_durak,varış))
 
 
 
-doğrudan2(ilk_durak,varış)
+doğrudan2(bulunulan_durak,ilk_durak,varış)
 
 
 
